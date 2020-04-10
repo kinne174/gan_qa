99db3156711cddad88ad9a843962975dbc7845a7
Mitch <kinne174@umn.edu>
2020-04-10 17:50:37 -0500

    added roberta model for multiple choice

--- utils_classifier.py
+++ utils_classifier.py
@@ -1,9 +1,6 @@
-import numpy as np
 import torch
 import torch.nn as nn
-from transformers import (BertForMultipleChoice, RobertaForMultipleChoice, XLMRobertaForMultipleChoice,
-                          BertConfig, RobertaConfig, XLMRobertaConfig, AlbertPreTrainedModel, AlbertConfig,
-                          AlbertModel)
+from transformers import (BertPreTrainedModel, RobertaConfig, AlbertPreTrainedModel, AlbertConfig, AlbertModel, RobertaModel)
 from transformers import PretrainedConfig
 import logging
 import os
@@ -53,7 +50,13 @@ class GeneralModelForMultipleChoice(nn.Module):
         batch_size = input_ids.shape[0]
 
         if 'inputs_embeds' in kwargs:
-            embeddings = self.albert.albert.embeddings.word_embeddings.weight.to(self.device)
+            if hasattr(self.model, 'roberta'):
+                embeddings = self.model.roberta.embeddings.word_embeddings.weight.to(self.device)
+            elif hasattr(self.model, 'albert'):
+                embeddings = self.model.albert.embeddings.word_embeddings.weight.to(self.device)
+            else:
+                raise NotImplementedError
+
             inputs_embeds = kwargs['inputs_embeds']
             assert inputs_embeds.is_sparse
             temp_inputs_embeds = torch.sparse.mm(inputs_embeds, embeddings)
@@ -255,6 +258,65 @@ def flip_labels(classification_labels, discriminator_labels, **kwargs):
     out_dict['discriminator_labels'] = out_d_labels
 
     return out_dict
+
+
+class RobertaForMultipleChoice(BertPreTrainedModel):
+    config_class = RobertaConfig
+    base_model_prefix = "roberta"
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.roberta = RobertaModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, 1)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        attention_mask=None,
+        labels=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+    ):
+
+        if input_ids is not None:
+            num_choices = input_ids.shape[1]
+            input_ids = input_ids.view(-1, input_ids.size(-1))
+        else:
+            num_choices = inputs_embeds.shape[1]
+            inputs_embeds = inputs_embeds.view(-1, inputs_embeds.shape[2], inputs_embeds.shape[3])
+
+        attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        reshaped_logits = logits.view(-1, num_choices)
+
+        outputs = (reshaped_logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(reshaped_logits, labels)
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
 
 
 class AlbertForMultipleChoice(AlbertPreTrainedModel):
