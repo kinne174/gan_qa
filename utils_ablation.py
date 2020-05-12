e601a2c368c880b45a1c28bae04c2c17584e1b6e
Mitch <kinne174@umn.edu>
2020-05-12 15:22:24 -0500

    added switched classifier inputs

--- utils_ablation.py
+++ utils_ablation.py
@@ -45,7 +45,7 @@ def translate_tokens(args, tokens):
 
 
 
-def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predictions, fake_predictions):
+def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predictions, fake_predictions, fake_predictions_switch):
     # print question and answers
     # for each answer print the windowed true and fake context and attention scores
     # can try to translate but not necessary on first pass
@@ -53,7 +53,9 @@ def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predi
     if not args.transformer_name in ['albert', 'roberta', 'bert']:
         raise NotImplementedError
 
-    assert fake_inputs['input_ids'].shape[1] == inputs['input_ids'].shape[1] == 4, 'One of fake_inputs 2nd dimension ({}) or inputs 2nd dimension ({}) is not 4'.format(fake_inputs['input_ids'].shape[1], inputs['input_ids'].shape[1])
+    assert fake_inputs['input_ids'].shape[1] == inputs['input_ids'].shape[1] == fake_inputs['input_ids_switch'].shape[1] == 4, \
+        'One of fake_inputs 2nd dimension ({}) or inputs 2nd dimension ({}) or fake_inputs switched ({}) is not 4'.format(
+            fake_inputs['input_ids'].shape[1], inputs['input_ids'].shape[1], fake_inputs['input_ids_switch'].shape[1])
 
     if os.path.exists(ablation_filename):
         write_append_trigger = 'a'
@@ -66,7 +68,9 @@ def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predi
             af.write('The two scores in parentheses are the score for the real and the fake respectively\n')
             af.write('\n*********************************************************************************\n')
 
-    assert inputs['input_ids'].shape[0] == fake_inputs['input_ids'].shape[0], 'inputs batch size ({}) is not the same as fake_inputs batch size ({})'.format(inputs['input_ids'].shape[0], fake_inputs['input_ids'].shape[0])
+    assert inputs['input_ids'].shape[0] == fake_inputs['input_ids'].shape[0] == fake_inputs['input_ids_switch'].shape[0], \
+        'inputs batch size ({}) is not the same as fake_inputs batch size ({}) is not the same as fake inputs switch batch size ({})'.format(
+            inputs['input_ids'].shape[0], fake_inputs['input_ids'].shape[0], fake_inputs['input_ids_switch'].shape[0])
     batch_size = inputs['input_ids'].shape[0]
 
     answer_letters = ['A.', 'B.', 'C.', 'D.']
@@ -86,6 +90,7 @@ def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predi
         all_answer_words = []
         all_real_words = []
         all_fake_words = []
+        all_fake_words_switch = []
         for j in range(4):
             seq_end_index = inputs['token_type_ids'][i, j, :].tolist().index(1)
             if args.transformer_name == 'roberta':
@@ -108,31 +113,37 @@ def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predi
                 fake_ids = fake_ids[i, j, seq_end_index:pad_index].long()
             else:
                 fake_ids = fake_inputs['input_ids'][i, j, seq_end_index:pad_index]
+                fake_ids_switch = fake_inputs['input_ids_switch'][i, j, seq_end_index:pad_index]
 
             real_words = tokenizer.convert_ids_to_tokens(real_ids)
             fake_words = tokenizer.convert_ids_to_tokens(fake_ids)
+            fake_words_switch = tokenizer.convert_ids_to_tokens(fake_ids_switch)
 
             my_attention_mask = fake_inputs['my_attention_mask'][i, j, seq_end_index:pad_index].tolist()
 
-            assert len(real_words) == len(fake_words) == len(my_attention_mask), 'Len of real_words ({}), len of fake_words ({}) and len of my_attention_mask ({}) does not match'.format(len(real_words),
-                                                                                                                                                                                          len(fake_words),
-                                                                                                                                                                                          len(my_attention_mask))
+            assert len(real_words) == len(fake_words) == len(fake_words_switch) == len(my_attention_mask), \
+                'Len of real_words ({}), len of fake_words ({}), len of fake_words_switch ({}) and len of my_attention_mask ({}) does not match'.format(
+                    len(real_words), len(fake_words), len(fake_words_switch), len(my_attention_mask))
 
             att_counter = 1
             new_fake_words = []
             new_real_words = []
+            new_fake_words_switch = []
             for k, att in enumerate(my_attention_mask):
                 if att == 1:
                     new_fake_words.extend(['*{}'.format(att_counter)] + [fake_words[k]] + ['*{}'.format(att_counter)])
                     new_real_words.extend(['*{}'.format(att_counter)] + [real_words[k]] + ['*{}'.format(att_counter)])
+                    new_fake_words_switch.extend(['*{}'.format(att_counter)] + [fake_words_switch[k]] + ['*{}'.format(att_counter)])
 
                     att_counter += 1
                 else:
                     new_fake_words.append(fake_words[k])
                     new_real_words.append(real_words[k])
+                    new_fake_words_switch.append(fake_words_switch[k])
 
             all_fake_words.append(new_fake_words)
             all_real_words.append(new_real_words)
+            all_fake_words_switch.append(new_fake_words_switch)
 
         current_real_label = inputs['classification_labels'][i, :]
         current_fake_label = 1 - current_real_label
@@ -149,26 +160,30 @@ def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predi
 
         real_softmaxed_scores = [round(ss, 3) for ss in sigmoid(real_predictions[i, :]).squeeze().tolist()]
         fake_softmaxed_scores = [round(ss, 3) for ss in sigmoid(fake_predictions[i, :]).squeeze().tolist()]
+        fake_softmaxed_scores_switch = [round(ss, 3) for ss in sigmoid(fake_predictions_switch[i, :]).squeeze().tolist()]
 
-        assert len(all_real_words) == len(all_fake_words) == len(real_predicted_label) == len(correct_real_label) == len(fake_predicted_label) == len(correct_fake_label) == len(real_softmaxed_scores) == len(fake_softmaxed_scores) == len(all_answer_words) == len(answer_letters)
-        answer_features = list(map(tuple, zip(all_real_words, all_fake_words, real_predicted_label, correct_real_label, fake_predicted_label, correct_fake_label, real_softmaxed_scores, fake_softmaxed_scores, all_answer_words, answer_letters)))
+        assert len(all_real_words) == len(all_fake_words) == len(all_fake_words_switch) == len(real_predicted_label) == len(correct_real_label) == len(fake_predicted_label) == len(correct_fake_label) == len(real_softmaxed_scores) == len(fake_softmaxed_scores) == len(fake_softmaxed_scores_switch) == len(all_answer_words) == len(answer_letters)
+        answer_features = list(map(tuple, zip(all_real_words, all_fake_words, all_fake_words_switch, real_predicted_label, correct_real_label, fake_predicted_label, correct_fake_label, real_softmaxed_scores, fake_softmaxed_scores, fake_softmaxed_scores_switch, all_answer_words, answer_letters)))
 
         with open(ablation_filename, write_append_trigger) as af:
             af.write('Predicted real answer: #r. Correct real answer: *r.\n')
-            af.write('Predicted fake wrong answer: #f. Correct fake wrong answer: *f.\n\n')
+            af.write('Predicted fake wrong answer: #f. Correct fake wrong answer: *f.\n')
+            af.write('Score of changing labels of generated words. \n\n')
 
             question_words = translate_tokens(args, question_words)
             af.write('** {}\n'.format(' '.join(question_words)))
 
-            for (rw, fw, rpl, crl, fpl, cfl, rss, fss, aw, al) in answer_features:
+            for (rw, fw, fws, rpl, crl, fpl, cfl, rss, fss, fssw, aw, al) in answer_features:
                 aw = translate_tokens(args, aw)
-                af.write('{} {} {} {} {} {} {}{}\n'.format(crl, rpl, rss, cfl, fpl, fss, al, ' '.join(aw)))
+                af.write('{} {} {} {} {} {}\t{} {}{}\n'.format(crl, rpl, rss, cfl, fpl, fss, fssw, al, ' '.join(aw)))
 
                 rw = translate_tokens(args, rw)
                 fw = translate_tokens(args, fw)
+                fws = translate_tokens(args, fws)
 
                 af.write('Real context: {}\n'.format(' '.join(rw)))
-                af.write('Fake context: {}\n\n'.format(' '.join(fw)))
+                af.write('Fake context: {}\n'.format(' '.join(fw)))
+                af.write('Fake context switch: {}\n\n'.format(' '.join(fws)))
 
             af.write('\n')
 
@@ -176,7 +191,7 @@ def ablation(args, ablation_filename, tokenizer, fake_inputs, inputs, real_predi
 
 
 def ablation_discriminator(args, ablation_filename, tokenizer, fake_inputs, inputs, fake_predictions, real_predictions, generator_predictions):
-    if not args.transformer_name in ['albert', 'roberta', 'bert']:
+    if args.transformer_name not in ['albert', 'roberta', 'bert']:
         raise NotImplementedError
 
     assert fake_inputs['input_ids'].shape[1] == inputs['input_ids'].shape[1] == 4, 'One of fake_inputs 2nd dimension ({}) or inputs 2nd dimension ({}) is not 4'.format(fake_inputs['input_ids'].shape[1], inputs['input_ids'].shape[1])
@@ -243,64 +258,6 @@ def ablation_discriminator(args, ablation_filename, tokenizer, fake_inputs, inpu
                     af.write('{}, {}\n'.format(rw, fw))
 
                 af.write('\n')
-
-
-def discriminator_eval(all_saved_data, oracleM, inputs):
-    num_of_grams = [2, 3, 4]
-
-    if not all_saved_data:
-
-        for n in num_of_grams:
-            all_saved_data['fake {}-gram counter'.format(n)] = Counter()
-            all_saved_data['real {}-gram counter'.format(n)] = Counter()
-
-        all_saved_data['all_perplexities'] = None
-
-        all_saved_data['all_fake_input_ids'] = None
-        all_saved_data['all_real_input_ids'] = None
-
-    assert 'masked_input_ids' in inputs
-    assert 'attention_mask' in inputs
-    assert 'token_type_ids' in inputs
-    assert 'classification_labels' in inputs
-    assert 'my_attention_mask' in inputs
-    assert 'fake_input_ids' in inputs
-    assert 'real_input_ids' in inputs
-    assert 'classification_labels' in inputs
-
-    with torch.no_grad():
-        oracle_vocabulary_logprobs = oracleM(inputs['masked_input_ids'], inputs['attention_mask'],
-                                             inputs['token_type_ids'], inputs['classification_labels'])
-        oracle_vocabulary_logprobs = oracle_vocabulary_logprobs.squeeze()
-
-        oracle_logprobs = oracle_vocabulary_logprobs.gather(dim=3, index=inputs['fake_input_ids'].unsqueeze(-1)).squeeze()
-        my_attention_mask = inputs['my_attention_mask'].view(-1, inputs['my_attention_mask'].shape[-1])
-        oracle_logprobs = oracle_logprobs.view(-1, oracle_logprobs.shape[-1])
-
-        current_perplexities = (torch.sum(oracle_logprobs * my_attention_mask, dim=-1) * -1) / torch.sum(
-            my_attention_mask, dim=-1)
-
-        if all_saved_data['all_perplexities'] is None:
-            all_saved_data['all_perplexities'] = current_perplexities
-        else:
-            all_saved_data['all_perplexities'] = torch.cat((all_saved_data['all_perplexities'], current_perplexities), dim=0)
-
-        inputs['fake_input_ids'] = inputs['fake_input_ids'].view(-1, inputs['fake_input_ids'].shape[-1])
-        inputs['real_input_ids'] = inputs['real_input_ids'].view(-1, inputs['real_input_ids'].shape[-1])
-
-        assert inputs['fake_input_ids'].shape[0] == inputs['real_input_ids'].shape[0]
-        for i in range(inputs['fake_input_ids'].shape[0]):
-            for n in num_of_grams:
-                all_saved_data['fake {}-gram counter'.format(n)].update(list(ngrams(inputs['fake_input_ids'][i, :].detach().cpu().tolist(), n)))
-                all_saved_data['real {}-gram counter'.format(n)].update(list(ngrams(inputs['real_input_ids'][i, :].detach().cpu().tolist(), n)))
-
-        if all_saved_data['all_fake_input_ids'] is None:
-            all_saved_data['all_fake_input_ids'] = inputs['fake_input_ids'].detach().cpu()
-        else:
-            all_saved_data['all_fake_input_ids'] = torch.cat((all_saved_data['all_fake_input_ids'],
-                                                              inputs['fake_input_ids'].detach().cpu()), dim=0)
-
-    return all_saved_data
 
 
 
